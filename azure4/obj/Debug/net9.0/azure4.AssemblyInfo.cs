23ed95d10df101a07a92d32cbf28e7820ae253aa
Harsh Gupta <harsh.gupta@newvisionsoftware.com>
2025-07-22 22:18:10 +0530

    Commit COnf

--- azure4/obj/Debug/net9.0/azure4.AssemblyInfo.cs
+++ azure4/obj/Debug/net9.0/azure4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("azure4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a26b211143b136487a5988bacd9e550a386d93b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b512d57f2d253d61c09b9c11430f3bc235c5301")]
 [assembly: System.Reflection.AssemblyProductAttribute("azure4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("azure4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
